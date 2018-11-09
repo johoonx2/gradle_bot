a3c8954ec27add45eaefd27318d84fd6b38f8f03
hoonjo <hoonjo@linecorp.com>
2018-11-09 16:24:35 +0900

    [조훈] jda 테스트

--- src/main/java/com/bot/controller/mainController.java
+++ src/main/java/com/bot/controller/mainController.java
@@ -1,26 +1,36 @@
 package com.bot.controller;
 
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandClientBuilder;
+import com.jagrosh.jdautilities.command.CommandEvent;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
-import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 
-public class mainController extends ListenerAdapter {
-    public static void main(String[] args) throws LoginException, RateLimitedException {
-        if(args.length > 0) {
-            new JDABuilder(AccountType.BOT)
-                    .setToken(args[0])
-                    .buildAsync();
-        }else {
-            System.out.println("Not found Token");
-        }
+public class mainController {
+    public static void main(String[] args) throws LoginException, RateLimitedException{
+        CommandClientBuilder commandClientBuilder = new CommandClientBuilder();
+        //our prefix is !!
+        commandClientBuilder.setPrefix("!!");
+        //"Type !!help"
+        commandClientBuilder.useDefaultGame();
+        commandClientBuilder.addCommand(new HelloWorldCommand());
+        new JDABuilder(AccountType.BOT)
+                .setToken(args[0])
+                .addEventListener(commandClientBuilder.build())
+                .buildAsync();
     }
-
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event){
-        System.out.println("Received!");
+    public static class HelloWorldCommand extends Command {
+        public HelloWorldCommand() {
+            this.name = "helloworld";
+            this.aliases = new String[]{"hw"};
+            this.help = "says hello";
+        }
+        @Override
+        protected void execute(CommandEvent commandEvent) {
+            commandEvent.reply("Hello world!");
+        }
     }
 }
